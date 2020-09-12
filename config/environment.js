e054b8150837223df9fa87b73c00eed4c1be64c8
Akhil PB <akhil.pb001@gmail.com>
2020-09-12 13:34:30 +0530

    Added app configurations

--- config/environment.js
+++ config/environment.js
@@ -1,11 +1,33 @@
-'use strict';
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-module.exports = function(environment) {
+/* jshint node: true */
+
+"use strict";
+
+const DEFAULT_CONFIG = require("./default-config");
+
+module.exports = function (environment) {
   let ENV = {
-    modulePrefix: 'yarn-ui',
+    modulePrefix: "yarn-ui",
     environment,
-    rootURL: '/',
-    locationType: 'hash',
+    rootURL: "/",
+    locationType: "hash",
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
@@ -13,17 +35,19 @@ module.exports = function(environment) {
       },
       EXTEND_PROTOTYPES: {
         // Prevent Ember Data from overriding Date.parse.
-        Date: false
-      }
+        Date: false,
+      },
     },
 
-    APP: {
-      // Here you can pass flags/options to your application instance
-      // when it is created
-    }
+    APP: DEFAULT_CONFIG,
+    contentSecurityPolicy: {
+      "connect-src": "* 'self'",
+      "child-src": "'self' 'unsafe-inline'",
+      "style-src": "'self' 'unsafe-inline'",
+    },
   };
 
-  if (environment === 'development') {
+  if (environment === "development") {
     // ENV.APP.LOG_RESOLVER = true;
     // ENV.APP.LOG_ACTIVE_GENERATION = true;
     // ENV.APP.LOG_TRANSITIONS = true;
@@ -31,19 +55,19 @@ module.exports = function(environment) {
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
   }
 
-  if (environment === 'test') {
+  if (environment === "test") {
     // Testem prefers this...
-    ENV.locationType = 'none';
+    ENV.locationType = "none";
 
     // keep test console output quieter
     ENV.APP.LOG_ACTIVE_GENERATION = false;
     ENV.APP.LOG_VIEW_LOOKUPS = false;
 
-    ENV.APP.rootElement = '#ember-testing';
+    ENV.APP.rootElement = "#ember-testing";
     ENV.APP.autoboot = false;
   }
 
-  if (environment === 'production') {
+  if (environment === "production") {
     // here you can enable a production-specific feature
   }
 
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,13 +1,47 @@
-'use strict';
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+"use strict";
 
-module.exports = function(defaults) {
-  let app = new EmberApp(defaults, {
-    'ember-bootstrap': {
+const EmberApp = require("ember-cli/lib/broccoli/ember-app");
+const Funnel = require("broccoli-funnel");
+
+module.exports = function (defaults) {
+  const isProd = EmberApp.env() === "production";
+
+  const app = new EmberApp(defaults, {
+    "ember-bootstrap": {
       bootstrapVersion: 4,
-      importBootstrapCSS: false
-    }
+      importBootstrapCSS: false,
+    },
+    "ember-cli-uglify": {
+      // Will be minified by wro4j-maven-plugin for performance
+      enabled: false,
+    },
+    minifyCSS: {
+      enabled: isProd,
+    },
+    fingerprint: {
+      enabled: false,
+    },
+    sourcemaps: {
+      enabled: !isProd,
+    },
   });
 
   // Use `app.import` to add additional libraries to the generated
@@ -23,5 +57,11 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
-  return app.toTree();
+  const extraAssets = new Funnel("config", {
+    srcDir: "/",
+    include: ["*.env"],
+    destDir: "/config",
+  });
+
+  return app.toTree(extraAssets);
 };
